efb5eca9a441d599553d74f7919953924c0458af
Dev-Simon20 <jeanpierks6@gmail.com>
2025-05-09 10:57:20 -0500

    config next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+/** @type {import('next').NextConfig} */
+import path from 'path';
+
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /libquery_engine-rhel-openssl-1.0.x.so.node$/,
+      loader: 'file-loader',
+      options: {
+        name: '[name].[ext]',
+        outputPath: 'prisma/generated/clientShop',
+        publicPath: '/prisma/generated/clientShop',
+      },
+    });
+    return config;
+  },
+};
 
 export default nextConfig;
